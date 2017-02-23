36849ff77867306375c3825ead77118a1f22f061
Walter A. Boring IV <waboring@hemna.com>
2017-02-23 17:06:10 -0500

    fix link to go to ~

--- link.sh
+++ link.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-DST=${1:-"`pwd`"}
+DST=$HOME
 ABSPATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 SRC=`dirname $ABSPATH`
 
 function link {
- rm $DST/$1
  ln -s $SRC/$1 $DST/$1
 }
 
