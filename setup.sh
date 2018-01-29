bcde285bc81f19f23f6ff60437c9f48bfc237cf0
Walter A. Boring IV <waboring@hemna.com>
2018-01-29 14:58:35 -0500

    add switching to zsh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 # use this to setup the basic shell and pull what is needed
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+chsh -s $(which zsh)
