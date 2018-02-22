43e8704c532ec0e1c1e3134345d120b17f103cf4
Walter A. Boring IV <waboring@hemna.com>
2018-02-22 13:58:38 -0500

    make sure .vim submodules are pulled in

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# make sure .vim submodules are installed
+cd .vim
+git submodule init
+git submodule update
+cd ..
+
 # use this to setup the basic shell and pull what is needed
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
