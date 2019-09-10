1422cb9ff730231dd715ebcbcc0054f127ebca3d
Walter A. Boring IV <waboring@hemna.com>
2019-09-10 21:36:32 +0200

    enable oh-my-fish

--- setup_fish.sh
+++ setup_fish.sh
@@ -8,15 +8,15 @@ if [ ! -d ~/.config/fish ]; then
     ln -s $SRC/fish ~/.config
 fi
 
-#if [ ! -d ~/.oh-my-fish ]; then
-#    git clone https://github.com/oh-my-fish/oh-my-fish
-#    cd oh-my-fish
-#    bin/install --path=~/.oh-my-fish
-#else
-#    cd ~/.oh-my-fish
-#    git pull origin
-#    git submodule update --init --recursive
-#fi
+if [ ! -d ~/.oh-my-fish ]; then
+    git clone https://github.com/oh-my-fish/oh-my-fish
+    cd oh-my-fish
+    bin/install --path=~/.oh-my-fish
+else
+    cd ~/.oh-my-fish
+    git pull origin
+    git submodule update --init --recursive
+fi
 
 # fish should be here now
 cd ~/.dotfiles
