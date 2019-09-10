f72b375f8498a03fbc04acd33f127de70cd829b5
Walter A. Boring IV <waboring@hemna.com>
2019-09-10 20:09:29 +0200

    comment out most of setup_fish.sh

--- setup_fish.sh
+++ setup_fish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 ABSPATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 SRC=`dirname $ABSPATH`
@@ -7,15 +8,15 @@ if [ ! -d ~/.config/fish ]; then
     ln -s $SRC/fish ~/.config
 fi
 
-if [ ! -d ~/.oh-my-fish ]; then
-    git clone https://github.com/oh-my-fish/oh-my-fish
-    cd oh-my-fish
-    bin/install --path=~/.oh-my-fish
-else
-    cd ~/.oh-my-fish
-    git pull origin
-    git submodule update --init --recursive
-fi
+#if [ ! -d ~/.oh-my-fish ]; then
+#    git clone https://github.com/oh-my-fish/oh-my-fish
+#    cd oh-my-fish
+#    bin/install --path=~/.oh-my-fish
+#else
+#    cd ~/.oh-my-fish
+#    git pull origin
+#    git submodule update --init --recursive
+#fi
 
 # fish should be here now
 cd ~/.dotfiles
