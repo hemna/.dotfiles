d9197f91800cc162ec3cbc786aa15cc567d60b4d
Walter A. Boring IV <waboring@hemna.com>
2019-09-11 00:36:47 +0200

    added unlink

---
+++ unlink.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+DST=$HOME
+ABSPATH="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
+SRC=`dirname $ABSPATH`
+
+function link {
+ if [ -e "$DST/$1" ]
+ then
+     rm $DST/$1
+ fi
+}
+
+link .aliases
+link .bashrc
+link .zshrc
+
+link .gitconfig
+link .screenrc
+link .vim
+link .vimrc
+link .tmux.conf
+link .bin
