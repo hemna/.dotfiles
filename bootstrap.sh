0c98288e39ec3d11ba98648031142355a979abb5
Walter A. Boring IV <waboring@hemna.com>
2019-09-10 16:30:53 +0000

    use master

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ cd ~
 echo "Clone the repo"
 git clone http://github.com/hemna/mydamnshell.git
 cd mydamnshell
-wget -O group_vars/all https://raw.githubusercontent.com/hemna/.dotfiles/ansible/group_vars/all
+wget -O group_vars/all https://raw.githubusercontent.com/hemna/.dotfiles/master/group_vars/all
 ./run.sh
